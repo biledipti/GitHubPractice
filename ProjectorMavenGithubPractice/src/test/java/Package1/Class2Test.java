a961326e1e38bf4bfac9a301dc62e840fc13eb2a
bilep <bilep@Pratik>
2025-12-03 17:31:46 +0530

    my first commit

---
+++ ProjectorMavenGithubPractice/src/test/java/Package1/Class2Test.java
@@ -0,0 +1,15 @@
+package Package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2Test {
+ 
+	
+	@Test
+	private void class1() {
+		
+	Reporter.log("*********** hey prachi good afternoon********",true);
+	}
+
+}
---
+++ ProjectorMavenGithubPractice/src/test/java/Package1/Class3Test.java
@@ -0,0 +1,15 @@
+package Package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+ 
+	
+	@Test
+	private void class1() {
+		
+	Reporter.log("*********** hey prachi good afternoon********",true);
+	}
+
+}
